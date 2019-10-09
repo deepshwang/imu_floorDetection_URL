a1c44d7e28523dfe8491113466c5f34f1978b318
LimHyungTae <shapelim@kaist.ac.kr>
2019-10-09 22:08:48 +0900

    Align codes and divide  'refiner' function into smaller functional unit.

--- csv_to_kaggleform.py
+++ csv_to_kaggleform.py
@@ -3,34 +3,45 @@ import numpy as np
 import os
 import time
 
-def name_collector():
-    path=os.getcwd()
-    filedir=os.listdir(path)
-    finaldir=[]
-    for name in filedir:
-        if name[:4]=="wood" or name[:4]=="tile" or name[:7]=="terazzo":
-            finaldir.append(name)
-    return finaldir
+CLASS_LIST = ["wood", 'tile', 'terazzo']
 
 
-def refiner(finaldir):
-    for name in finaldir:
-        df=pd.read_csv(name)
-        df=df.drop(["time",".header.seq", ".header.stamp.secs", ".header.stamp.nsecs", ".header.frame_id", ".orientation_covariance", ".angular_velocity_covariance", ".linear_acceleration_covariance"], axis=1)
-        df=df.rename(columns={'.orientation.x': 'orientation_X', '.orientation.y': 'orientation_Y', '.orientation.z': 'orientation_Z', '.orientation.w': 'orientation_W', '.angular_velocity.x': 'angular_velocity_X', '.angular_velocity.y': 'angular_velocity_Y', '.angular_velocity.z': 'angular_velocity_Z', '.linear_acceleration.x': 'linear_acceleration_X', '.linear_acceleration.y': 'linear_acceleration_Y', '.linear_acceleration.z': 'linear_acceleration_Z'}, errors='raise')
+def get_raw_csv(dir):
+    candidates = os.listdir(dir)
+    target_csv_list = []
+    for name in candidates:
+        if name[:4] in CLASS_LIST or name[:7] in CLASS_LIST:
+            csv_path = os.path.join(dir, name)
+            target_csv_list.append(csv_path)
+    return target_csv_list
 
 
-        df = df[df.angular_velocity_Z != 0]
-        df.index = np.arange(0, len(df))
-        df['measurement_number']=df.index%129
-        df['series_id']=df.index//129
-        df['row_id']=df['series_id'].map(str)+"_"+df["measurement_number"].map(str)
+def refine_csv(src_path):
+    df = pd.read_csv(src_path)
+    df = df.drop(["time", ".header.seq", ".header.stamp.secs", ".header.stamp.nsecs", ".header.frame_id", ".orientation_covariance", ".angular_velocity_covariance", ".linear_acceleration_covariance"], axis=1)
+    # df=df.rename(columns={'.orientation.x': 'orientation_X', '.orientation.y': 'orientation_Y', '.orientation.z': 'orientation_Z', '.orientation.w': 'orientation_W', '.angular_velocity.x': 'angular_velocity_X', '.angular_velocity.y': 'angular_velocity_Y', '.angular_velocity.z': 'angular_velocity_Z', '.linear_acceleration.x': 'linear_acceleration_X', '.linear_acceleration.y': 'linear_acceleration_Y', '.linear_acceleration.z': 'linear_acceleration_Z'}, errors='raise')
+    df = df.rename(columns={'.orientation.x': 'orientation_X', '.orientation.y': 'orientation_Y', '.orientation.z': 'orientation_Z', '.orientation.w': 'orientation_W', '.angular_velocity.x': 'angular_velocity_X', '.angular_velocity.y': 'angular_velocity_Y', '.angular_velocity.z': 'angular_velocity_Z', '.linear_acceleration.x': 'linear_acceleration_X', '.linear_acceleration.y': 'linear_acceleration_Y', '.linear_acceleration.z': 'linear_acceleration_Z'})
 
-        new_cols=["row_id", "series_id", "measurement_number"]+df.columns.tolist()[:10]
-        df=df[new_cols]
+    df = df[df.angular_velocity_Z != 0]
+    df.index = np.arange(0, len(df))
+    df['measurement_number'] = df.index % 129
+    df['series_id'] = df.index//129
+    df['row_id'] = df['series_id'].map(str) + "_" + df["measurement_number"].map(str)
 
-        df.to_csv("refined_"+name)
+    new_cols = ["row_id", "series_id", "measurement_number"] + df.columns.tolist()[:10]
+    df = df[new_cols]
+    if not "/" in src_path:
+        df.to_csv("refined" + src_path)
+    else:
+        src_name = src_path.split("/")[-1]
+        df.to_csv("refined" + src_name)
 
-##Function Operation
-refiner(name_collector())
-print ("Refining COMPLETE!")
+
+if __name__ == "__main__":
+    target_dir = "raw_data"
+    csv_list = get_raw_csv(target_dir)
+    print(csv_list)
+
+    for csv_path in csv_list:
+        refine_csv(csv_path)
+    print("Refining COMPLETE!")
