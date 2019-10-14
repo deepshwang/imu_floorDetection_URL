e40fe2825fb093e870240c2276197d9a483d29bf
LimHyungTae <shapelim@kaist.ac.kr>
2019-10-14 10:34:03 +0900

    Upload analyzed data

--- data_visualization.py
+++ data_visualization.py
@@ -21,7 +21,7 @@ def plot_dataframe_for_columns(column_name, pd_data_list):
         datapoints = pd_data[column_name]
         plt.plot(range(datapoints.count()), datapoints, label=LABEL[i])
 
-    plt.ylim(-0.1, 0.1)
+    # plt.ylim(-0.1, 0.1)
     plt.legend()
     plt.xlabel("Time stamp")
     plt.ylabel("vel_z")
@@ -41,6 +41,7 @@ def plot_fft_for_columns(column_name, pd_data_list):
 
     plt.legend()
     plt.xlabel("Frequnecy (Hz)")
+    # plt.xlim(0, 2)
     plt.ylabel("Magnitude")
     title="Freq. range of <"+ column_name+">"
     plt.title(title)
@@ -50,9 +51,27 @@ def plot_fft_for_columns(column_name, pd_data_list):
     plt.show()
 
 
+def plot_fft_w_partial_data(column_name, pd_data_list, initial_point, end_point):
+    for i, pd_data in enumerate(pd_data_list):
+        datapoints=pd_data[column_name].to_numpy()[initial_point:end_point]
+        fft_datapoints=abs(np.fft.fft(datapoints))[0:len(datapoints)]
+        Fq=20  ### Sampling rate is 20Hz
+        approx_freq_range=[x*Fq/len(datapoints) for x in range(len(datapoints))]  # Reference: https://kr.mathworks.com/help/matlab/ref/fft.html
+        half_length=len(approx_freq_range)//2
+        plt.plot(approx_freq_range[0:half_length], fft_datapoints[0:half_length], label=LABEL[i])
 
 
-#################    MAIN OPERATION    ##################
+    plt.legend()
+    plt.xlabel("Frequnecy (Hz)")
+    # plt.xlim(0, 2)
+    plt.ylabel("Magnitude")
+    title="Freq. range of <"+ column_name+">"
+    plt.title(title)
+    fig=plt.gcf()
+    filename="DFT_freq_range_"+column_name+ str(initial_point) + "_to_" + str(end_point) + ".png"
+    fig.savefig(filename)
+    plt.show()
+
 
 
 if __name__ == "__main__":
@@ -60,7 +79,10 @@ if __name__ == "__main__":
                     "refined_result/refined_tile_2.csv",
                     "refined_result/refined_wood_2.csv"]
 
+    plt.rcParams["figure.figsize"] = (20, 15)
+
     pd_data_list=csv_to_dataframe_in_list(target_files)
 
-    #plot_dataframe_for_column('angular_velocity_Z', pd_data_list)
-    plot_fft_for_columns('angular_velocity_Z',pd_data_list)
+    # plot_dataframe_for_columns('angular_velocity_Z', pd_data_list)
+    # plot_fft_for_columns('angular_velocity_Z', pd_data_list)
+    plot_fft_w_partial_data('angular_velocity_Z', pd_data_list, 700, 830)
